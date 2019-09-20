264d6d1db1d63231e5b6916c3037d859fa9fd47c
albrdev <albrdev@gmail.com>
2019-09-20 13:53:00 +0200

    implemented udp client

--- marvin42-dacapo-joystick_client.ino
+++ marvin42-dacapo-joystick_client.ino
@@ -1,41 +1,85 @@
 #include <Arduino.h>
+#include "src/crc.h"
+#include "src/packet.h"
+#include "src/custom_packets.h"
+#include "InterruptIn.hpp"
+#include "src/generic.hpp"
 
-#define normalize01(x, min, max) (((x) - (min)) / ((max) - (min)))                          // (x - min) / (max - min)
-#define normalize11(x, min, max) (((x) - (((min) + (max)) / 2)) / (((max) - (min)) / 2))    // (x - ((min + max) / 2)) / ((max - min) / 2)
-#define denormalize01(x, min, max) (((x) * ((max) - (min))) + (min))                        // (x * ((max) - min)) + (min)
-#define denormalize11(x, min, max) (((x) + (((min) + (max)) / 2)) * (((max) - (min)) / 2))  // (x + ((min + max) / 2)) * ((max - min) / 2)
-
-int xVal;
-int yVal;
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#ifndef STASSID
+#define STASSID "ssid"
+#define STAPSK  "password"
+#endif
 
 unsigned long delayTime = 100;
 const unsigned int MAX = 1024 + 1;
 
+WiFiUDP client;
+
+#define ADDRESS "192.168.10.33"
+#define PORT    1042
+
+InterruptIn pauseButton(D3);
+
+bool isPaused = false;
+void onPushPauseButton(const bool value)
+{
+    if(!value)
+        return;
+
+    isPaused = !isPaused;
+    Serial.println(isPaused ? "Paused" : "Unpaused");
+}
 
 void setup()
 {
     Serial.begin(115200);
 
-    randomSeed(analogRead(0));
+    randomSeed(analogRead(0));//*
 
     // Initialize the output variables as outputs
     pinMode(A0, INPUT);
     pinMode(RX, INPUT);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(STASSID, STAPSK);
+    while(WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print('.');
+        delay(500);
+    }
+
+    Serial.print("Connected! IP address: ");
+    Serial.println(WiFi.localIP());
+    client.begin(PORT);
+
+    pauseButton.SetOnStateChangedCallback(onPushPauseButton);
 }
 
 void loop()
 {
-    xVal = random(0, MAX);
-    yVal = random(0, MAX);
-    //xVal = analogRead(A0);
-    //yVal = analogRead(RX);
+    pauseButton.Poll();
+    if(isPaused)
+        return;
 
-    float x = normalize11((double)xVal, 0, MAX);
-    float y = normalize11((double)yVal, 0, MAX);
+    //int axisX = random(0, MAX); // Debug
+    //int axisY = random(0, MAX); // Debug
+    int axisX = analogRead(A0);
+    int axisY = analogRead(RX);
 
-    Serial.print("x="); Serial.print(x);
-    Serial.print(", y="); Serial.println(y);
+    float normX = normalize11((float)axisX, 0, MAX);
+    float normY = normalize11((float)axisY, 0, MAX);
 
+    Serial.print("x="); Serial.print(normX);
+    Serial.print(", y="); Serial.println(normY);
+
+    packet_motorrun_t pkt;
+    packet_mkmotorrun(&pkt, normX, normY);
+
+    client.beginPacket(ADDRESS, PORT);
+    client.write((const char*)&pkt, sizeof(pkt));
+    client.endPacket();
 
     delay(delayTime);
 }
