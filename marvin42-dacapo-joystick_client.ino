70920905b945d8099fbb24d2fbc3b154e4caa218
albrdev <albrdev@gmail.com>
2019-11-25 14:24:16 +0100

    main code reworked

--- marvin42-dacapo-joystick_client.ino
+++ marvin42-dacapo-joystick_client.ino
@@ -1,159 +1,82 @@
 #include <math.h>
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
 #include "crc.h"
 #include "packet.h"
 #include "custom_packets.h"
-#include "InterruptIn.hpp"
-#include "RotaryEncoder.hpp"
+#include "Button.hpp"
+#include "Joystick.hpp"
 #include "generic.hpp"
 
 #include "config.h" // IP/port, WiFi SSID/password
 
 const unsigned long delayTime = 500;
 
-WiFiUDP client;
-RotaryEncoder rotaryEncoder(D0, D1, D4);
-//InterruptIn pauseButton(D3);
+#define D2 2
 
-int currentCount = 0;
-int8_t direction = 0;
-float balance = 0.0f;
-float lastBalance = 0.0f;
-int8_t lastDirection = -2;
+Joystick joystick(A0, A1, D2, 0.05);
 
-//bool isPaused = false;
+float lastX = 0.0f;
+float lastY = 0.0f;
 
 void onJoystickButtonPressed(const bool value)
 {
     if(value)
     {
         //PrintDebugLine("Joystick button released");
-        PrintDebugLine("Reset balance");
-        currentCount = 0;
+        PrintDebugLine("Joystick button");
     }
     /*else
     {
         //PrintDebugLine("Joystick button pressed");
     }*/
 
-    //delay(100);
+    delay(250);
 }
-
-void onRotationChanged(const int value, const bool cw)
-{
-    if(cw)
-    {
-        if(currentCount >= RotaryEncoder::MaxCount)
-        {
-            PrintDebugLine("Max. value reached: ");
-            return;
-        }
-    }
-    else
-    {
-        if(currentCount <= -RotaryEncoder::MaxCount)
-        {
-            PrintDebugLine("Min. value reached: ");
-            return;
-        }
-    }
-
-    currentCount += cw ? -1 : 1;
-    PrintDebug("Counter: "); PrintDebug(currentCount); PrintDebug(", Direction: "); PrintDebugLine(cw ? "CW" : "CCW");
-    //delay(100);
-}
-
-/*void onPauseButtonPressed(const bool value)
-{
-    if(!value)
-        return;
-
-    isPaused = !isPaused;
-    PrintDebugLine(isPaused ? "Paused" : "Unpaused");
-    delay(150);
-}*/
 
 void setup(void)
 {
     delay(2500);
     Serial.print("Initializing...");
-    Serial.begin(115200);
+    Serial.begin(9600);
 
-    //randomSeed(analogRead(0));//*
-
-    // Initialize the output variables as outputs
-    pinMode(D8, INPUT); // Inbuilt 10k pulldown
-    pinMode(D3, INPUT); // Inbuilt 10k pullup
-
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(SSID, PSK);
-    while(WiFi.status() != WL_CONNECTED)
-    {
-        Serial.print('.');
-        delay(500);
-    }
-
-    Serial.print("Connected! IP address: ");
-    Serial.println(WiFi.localIP());
-    client.begin(PORT);
-
-    rotaryEncoder.SetOnValueChangedEvent(onRotationChanged);
-    rotaryEncoder.SetOnStateChangedEvent(onJoystickButtonPressed);
-    //pauseButton.SetOnStateChangedEvent(onPauseButtonPressed);
+    joystick.SetOnStateChangedEvent(onJoystickButtonPressed);
 
     Serial.println("Done");
 }
 
-void SendMotorJSDataPacket(const float balance, const float direction)
+void SendMotorRunPacket(const float x, const float y)
 {
-    packet_motorjsdata_t pkt;
-    packet_mkmotorjsdata(&pkt, balance, direction);
+    packet_motorrun_t pkt;
+    packet_mkmotorrun(&pkt, x, y);
 
-    client.beginPacket(ADDRESS, PORT);
-    client.write((const char*)&pkt, sizeof(pkt));
-    client.endPacket();
+    // TODO: Send
 }
 
 void SendMotorStopPacket(void)
 {
-    packet_motorjsdata_t pkt;
+    packet_header_t pkt;
     packet_mkbasic(&pkt, CPT_MOTORSTOP);
 
-    client.beginPacket(ADDRESS, PORT);
-    client.write((const char*)&pkt, sizeof(pkt));
-    client.endPacket();
+    // TODO: Send
 }
 
 void loop(void)
 {
-    /*pauseButton.Poll();
-    if(isPaused)
-        return;*/
+    joystick.Poll();
+    float x = joystick.GetX();
+    float y = joystick.GetY();
+    //PrintDebug("Input: "); PrintDebug("x="); PrintDebug(x); PrintDebug(", y="); PrintDebugLine(y);
 
-    uint8_t forwardsButton = digitalRead(D8);
-    uint8_t backwardsButton = digitalRead(D3);
-
-    if(forwardsButton)
-        direction = 1;
-    else if(!backwardsButton)
-        direction = -1;
-
-    rotaryEncoder.Poll();
-    balance = normalize11((float)currentCount, -RotaryEncoder::MaxCount, RotaryEncoder::MaxCount);
-    //PrintDebug("Input: "); PrintDebug("balance="); PrintDebug(balance); PrintDebug(", direction="); PrintDebugLine(direction);
-
-    if(absdiff(balance, lastBalance) <= 0.05f && direction == lastDirection)
+    if(absdiff(x, lastX) <= 0.05f && absdiff(y, lastY) <= 0.05f)
     {
-        //Serial.println("Deadzone");
         return;
     }
 
-    PrintDebug("Sending: "); PrintDebug("balance="); PrintDebug(balance); PrintDebug(", direction="); PrintDebugLine(direction);
-    SendMotorJSDataPacket(balance, direction);
+    //PrintDebug("Sending: "); PrintDebug("x="); PrintDebug(x); PrintDebug(", y="); PrintDebugLine(y);
+    SendMotorRunPacket(x, y);
 
-    lastBalance = balance;
-    lastDirection = direction;
+    lastX = x;
+    lastY = y;
+
     //delay(delayTime);
 }
