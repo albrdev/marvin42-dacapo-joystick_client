c30fac6d1cfdf448a7a0d722f0037b0379f55f64
albrdev <albrdev@gmail.com>
2019-11-25 14:22:01 +0100

    small fixes

--- inc/Joystick.hpp
+++ inc/Joystick.hpp
@@ -2,18 +2,20 @@
 #define _JOYSTICK_HPP_
 
 #include <stdint.h>
+#include "Button.hpp"
 
-class Joystick {
+class Joystick : public Button
+{
     public:
-    Joystick(uint8_t pinX, uin8_t pinY, float deadzone = 0.05f);
-    float GetX();
-    float GetY();
+    Joystick(const uint8_t pinX, const uint8_t pinY, const uint8_t buttonPin, const float deadzone = 0.05f);
+    float GetX(void) const;
+    float GetY(void) const;
 
     private:
-    float m_Deadzone;
     uint8_t m_PinX;
     uint8_t m_PinY;
 
+    float m_Deadzone;
 };
 
 #endif
--- src/Joystick.cpp
+++ src/Joystick.cpp
@@ -2,23 +2,20 @@
 #include <Arduino.h>
 #include "maths.h"
 
-float Joystick::GetX(){
+float Joystick::GetX(void) const
+{
     float value = normalize11((float)analogRead(m_PinX), 0, 1023);
     return fabs(value) >= m_Deadzone ? value : 0.0f;
 }
 
-float Joystick::GetY(){
+float Joystick::GetY(void) const
+{
     float value = normalize11((float)analogRead(m_PinY), 0, 1023);
     return fabs(value) >= m_Deadzone ? value : 0.0f;
 }
 
-Joystick::Joystick(uint8_t pinX, uin8_t pinY, float deadzone)
+Joystick::Joystick(const uint8_t pinX, const uint8_t pinY, const uint8_t buttonPin, const float deadzone) : Button(buttonPin), m_PinX(pinX), m_PinY(pinY), m_Deadzone(deadzone)
 {
-    m_PinX = pinX;
-    m_PinY = pinY;
-
-    m_Deadzone = deadzone;
-
     pinMode(m_PinX, INPUT);
     pinMode(m_PinY, INPUT);
 }
