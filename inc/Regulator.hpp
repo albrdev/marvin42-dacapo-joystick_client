5815f86ba68b9f98159434e77237730eb3b889d9
albrdev <albrdev@gmail.com>
2019-11-25 16:18:04 +0100

    regulator class

---
+++ inc/Regulator.hpp
@@ -0,0 +1,32 @@
+#ifndef __REGULATOR_HPP__
+#define __REGULATOR_HPP__
+
+#include <stdint.h>
+
+class Regulator
+{
+public:
+    typedef void(*OnValueChangedEventHandler)(const float, const float);
+
+private:
+    uint8_t m_Pin;
+
+    float m_LowerCap;
+    float m_UpperCap;
+    float m_Sensitivity;
+
+    float m_Value = 0.0f;
+
+    OnValueChangedEventHandler m_OnValueChangedEvent = nullptr;
+
+public:
+    float GetValue(void) const;
+
+    void SetOnValueChangedEvent(const OnValueChangedEventHandler value);
+
+    virtual void Poll(void);
+
+    Regulator(const uint8_t pin, const float lowerCap = 0.0f, const float upperCap = 1.0f, const float sensitivity = 0.0f);
+};
+
+#endif // __REGULATOR_HPP__
---
+++ src/Regulator.cpp
@@ -0,0 +1,34 @@
+#include "Regulator.hpp"
+#include <Arduino.h>
+#include "maths.h"
+#include "generic.hpp"
+
+float Regulator::GetValue(void) const
+{
+    float value = normalize01((float)analogRead(m_Pin), 0, ANALOG_MAX);
+    return value < m_LowerCap ? 0.0f : (value > m_UpperCap ? 1.0f : value);
+}
+
+void Regulator::SetOnValueChangedEvent(const OnValueChangedEventHandler value)
+{
+    m_OnValueChangedEvent = value;
+}
+
+void Regulator::Poll(void)
+{
+    float value = GetValue();
+    if(absdiff(value, m_Value) >= m_Sensitivity)
+    {
+        if(m_OnValueChangedEvent != nullptr)
+        {
+            m_OnValueChangedEvent(m_Value, value);
+        }
+
+        m_Value = value;
+    }
+}
+
+Regulator::Regulator(const uint8_t pin, const float lowerCap, const float upperCap, const float sensitivity) : m_Pin(pin), m_LowerCap(lowerCap), m_UpperCap(upperCap), m_Sensitivity(sensitivity)
+{
+    pinMode(pin, INPUT);
+}
