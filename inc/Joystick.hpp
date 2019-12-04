7a059b5bd047787e90712f0c9a061b7703f48d9d
albrdev <albrdev@gmail.com>
2019-12-04 16:43:27 +0100

    added axis callback and tolerance

--- inc/Joystick.hpp
+++ inc/Joystick.hpp
@@ -6,16 +6,29 @@
 
 class Joystick : public Button
 {
-    public:
-    Joystick(const uint8_t pinX, const uint8_t pinY, const uint8_t buttonPin, const float deadzone = 0.05f);
+public:
+    typedef void(*OnAxisChangedEventHandler)(const float, const float);
+
+    Joystick(const uint8_t pinX, const uint8_t pinY, const uint8_t buttonPin, const float deadzone = 0.05f, const float tolerance = 0.0f);
+
+    virtual void Poll(void) override;
+
+    void SetOnAxisChangedEvent(const OnAxisChangedEventHandler value);
+
     float GetX(void) const;
     float GetY(void) const;
 
-    private:
+private:
     uint8_t m_PinX;
     uint8_t m_PinY;
 
     float m_Deadzone;
+    float m_Tolerance;
+
+    OnAxisChangedEventHandler m_OnAxisChangedEvent = nullptr;
+
+    float m_X = 0.0f;
+    float m_Y = 0.0f;
 };
 
 #endif // __JOYSTICK_HPP__
--- src/Joystick.cpp
+++ src/Joystick.cpp
@@ -15,7 +15,30 @@ float Joystick::GetY(void) const
     return fabs(value) >= m_Deadzone ? value : 0.0f;
 }
 
-Joystick::Joystick(const uint8_t pinX, const uint8_t pinY, const uint8_t buttonPin, const float deadzone) : Button(buttonPin), m_PinX(pinX), m_PinY(pinY), m_Deadzone(deadzone)
+void Joystick::SetOnAxisChangedEvent(const OnAxisChangedEventHandler value)
+{
+    m_OnAxisChangedEvent = value;
+}
+
+void Joystick::Poll(void)
+{
+    float x = GetX();
+    float y = GetY();
+    if(absdiff(x, m_X) >= m_Tolerance || absdiff(y, m_Y) >= m_Tolerance)
+    {
+        if(m_OnAxisChangedEvent != nullptr)
+        {
+            m_OnAxisChangedEvent(x, y);
+        }
+
+        m_X = x;
+        m_Y = y;
+    }
+
+    Button::Poll();
+}
+
+Joystick::Joystick(const uint8_t pinX, const uint8_t pinY, const uint8_t buttonPin, const float deadzone, const float tolerance) : Button(buttonPin), m_PinX(pinX), m_PinY(pinY), m_Deadzone(deadzone), m_Tolerance(tolerance)
 {
     pinMode(m_PinX, INPUT);
     pinMode(m_PinY, INPUT);
