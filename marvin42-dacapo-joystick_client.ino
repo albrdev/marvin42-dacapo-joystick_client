cacd17a2af9f6b42eaccb1467b41b391afd419ef
albrdev <albrdev@gmail.com>
2019-12-04 16:43:56 +0100

    fixed code for testing

--- marvin42-dacapo-joystick_client.ino
+++ marvin42-dacapo-joystick_client.ino
@@ -3,35 +3,70 @@
 #include "crc.h"
 #include "packet.h"
 #include "custom_packets.h"
+#include "HC06.hpp"
 #include "Button.hpp"
 #include "Joystick.hpp"
 #include "Regulator.hpp"
 #include "generic.hpp"
+#include <SoftwareSerial.h>
 
 #include "config.h" // IP/port, WiFi SSID/password
 
 #define D2 2
 #define D3 3
+#define D10 10
+#define D11 11
 
 const unsigned long delayTime = 500;
 
-Joystick leftJoystick(A0, A1, D2, 0.05);
-Joystick rightJoystick(A2, A3, D3, 0.05);
-Regulator speedRegulator(A4, 22.0f / ANALOG_MAX, 1000.0f / ANALOG_MAX, 0.005);
+//HC06 bluetooth(D10, D11);
+SoftwareSerial bluetooth(D10, D11);
+Joystick leftJoystick(A0, A1, D2, 0.1f, 0.025f);
+Joystick rightJoystick(A2, A3, D3, 0.1f, 0.025f);
+Regulator speedRegulator( A4, 31.0f / ANALOG_MAX, 1000.0f / ANALOG_MAX, 0.01f);
 
-float lastX = 0.0f;
-float lastY = 0.0f;
+struct
+{
+    vector2data_t direction;
+    float power;
+} inputdata = { { 0.0f, 0.0f }, 0.0f };
+
+void SendDirectionPacket(void)
+{
+    packet_direction_t pkt;
+    packet_mkdirection(&pkt, &inputdata.direction);
+
+    //bluetooth.Write((const uint8_t*)&pkt, sizeof(pkt));
+    bluetooth.write((const uint8_t*)&pkt, sizeof(pkt));
+}
+
+void SendMotorPowerPacket(void)
+{
+    packet_motorpower_t pkt;
+    packet_mkmotorpower(&pkt, inputdata.power);
+
+    //bluetooth.Write((const uint8_t*)&pkt, sizeof(pkt));
+    bluetooth.write((const uint8_t*)&pkt, sizeof(pkt));
+}
+
+void SendMotorStopPacket(void)
+{
+    packet_header_t pkt;
+    packet_mkbasic(&pkt, CPT_MOTORSTOP);
+
+    //bluetooth.Write((const uint8_t*)&pkt, sizeof(pkt));
+    bluetooth.write((const uint8_t*)&pkt, sizeof(pkt));
+}
 
 void onLeftJoystickButtonPressed(const bool value)
 {
     if(value)
     {
-        //PrintDebugLine("Joystick button released");
-        PrintDebugLine("Left joystick button");
+        PrintDebug("Joystick(Left): Released");
     }
     /*else
     {
-        //PrintDebugLine("Joystick button pressed");
+        PrintDebug("Joystick(Left): Pressed");
     }*/
 
     delay(250);
@@ -41,52 +76,120 @@ void onRightJoystickButtonPressed(const bool value)
 {
     if(value)
     {
-        //PrintDebugLine("Joystick button released");
-        PrintDebugLine("Right joystick button");
+        PrintDebug("Joystick(Right): Released");
     }
     /*else
     {
-        //PrintDebugLine("Joystick button pressed");
+        PrintDebug("Joystick(Right): Pressed");
     }*/
 
     delay(250);
 }
 
+void onLeftJoystickAxisChanged(const float x, const float y)
+{
+    inputdata.direction.x = -x;
+    inputdata.direction.y = -y;
+
+    PrintDebug("Joystick(Left): ");
+    PrintDebug("x="); PrintDebug(inputdata.direction.x); PrintDebug(", ");
+    PrintDebug("y="); PrintDebug(inputdata.direction.y);
+    PrintDebugLine("");
+
+    SendDirectionPacket();
+}
+
+void onRightJoystickAxisChanged(const float x, const float y)
+{
+    PrintDebug("Joystick(Right): ");
+    PrintDebug("x="); PrintDebug(x); PrintDebug(", ");
+    PrintDebug("y="); PrintDebug(y);
+    PrintDebugLine("");
+}
+
 void onSpeedRegulated(const float oldValue, const float newValue)
 {
-    int dir = sgn(newValue - oldValue);
-    PrintDebug("Speed: "); PrintDebug(dir > 0 ? "Increased to " : "Decreased to "); PrintDebugLine(newValue);
+    inputdata.power = newValue;
+    int dir = sgn(inputdata.power - oldValue);
+
+    PrintDebug("Speed: ");
+    PrintDebug(dir > 0 ? "Increased to " : "Decreased to "); PrintDebug(inputdata.power);
+    PrintDebugLine("");
+
+    SendMotorPowerPacket();
 }
+
+/*void setupBluetooth(void)
+{
+    Serial.println("Initializing Bluetooth device...");
+    bluetooth.Begin(HC06::BR_9600);
+
+    Serial.println("Verifying device");
+    while(!bluetooth.Ping())
+    {
+        Serial.println("Failed");
+        delay(500UL);
+    }
+
+    delay(500UL);
+    Serial.print("Version: ");
+    const char* version = bluetooth.GetVersion();
+    if(version != nullptr)
+    {
+        Serial.println(version);
+    }
+    else
+    {
+        Serial.println("N/A");
+    }
+
+    delay(500UL);
+    Serial.println("Setting baud rate");
+
+    while(!bluetooth.SetBaudRate(HC06::BR_9600))
+    {
+        Serial.println("Failed");
+        delay(500UL);
+    }
+
+    delay(500UL);
+    Serial.println("Setting PIN");
+    while(!bluetooth.SetPIN("1357"))
+    {
+        Serial.println("Failed");
+        delay(500UL);
+    }
+
+    delay(500UL);
+    Serial.println("Setting name");
+    while(!bluetooth.SetName("Marvin42"))
+    {
+        Serial.println("Failed");
+        delay(500UL);
+    }
+
+    Serial.println("");
+}*/
 
 void setup(void)
 {
-    Serial.begin(9600);
+    Serial.begin(9600, SERIAL_8N1);
     Serial.println("Initializing...");
     Serial.flush();
     delay(2500);
 
+    Serial.println("Initializing input device...");
+    leftJoystick.SetOnAxisChangedEvent(onLeftJoystickAxisChanged);
     leftJoystick.SetOnStateChangedEvent(onLeftJoystickButtonPressed);
+    rightJoystick.SetOnAxisChangedEvent(onRightJoystickAxisChanged);
     leftJoystick.SetOnStateChangedEvent(onRightJoystickButtonPressed);
     speedRegulator.SetOnValueChangedEvent(onSpeedRegulated);
 
+    //setupBluetooth();
+    bluetooth.begin(9600);
+
     Serial.println("Done");
     Serial.flush();
-}
-
-void SendMotorRunPacket(const float x, const float y)
-{
-    packet_motorrun_t pkt;
-    packet_mkmotorrun(&pkt, x, y);
-
-    // TODO: Send
-}
-
-void SendMotorStopPacket(void)
-{
-    packet_header_t pkt;
-    packet_mkbasic(&pkt, CPT_MOTORSTOP);
-
-    // TODO: Send
 }
 
 void loop(void)
@@ -94,24 +197,6 @@ void loop(void)
     leftJoystick.Poll();
     rightJoystick.Poll();
     speedRegulator.Poll();
-
-    float x1 = leftJoystick.GetX();
-    float y1 = leftJoystick.GetY();
-    float x2 = rightJoystick.GetX();
-    float y2 = rightJoystick.GetY();
-    //PrintDebug("Joystick(1): "); PrintDebug("x="); PrintDebug(x1); PrintDebug(", y="); PrintDebugLine(y1);
-    //PrintDebug("Joystick(2): "); PrintDebug("x="); PrintDebug(x2); PrintDebug(", y="); PrintDebugLine(y2);
-
-    if(absdiff(x1, lastX) <= 0.05f && absdiff(y1, lastY) <= 0.05f)
-    {
-        return;
-    }
-
-    //PrintDebug("Sending: "); PrintDebug("x="); PrintDebug(x); PrintDebug(", y="); PrintDebugLine(y);
-    SendMotorRunPacket(x1, y1);
-
-    lastX = x1;
-    lastY = y1;
 
     //delay(delayTime);
 }
