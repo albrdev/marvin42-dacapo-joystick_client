5cece28ba89ec63eaaf1812d0f07ab81b39449c0
albrdev <albrdev@gmail.com>
2019-12-16 15:28:12 +0100

    comments++

--- inc/Button.hpp
+++ inc/Button.hpp
@@ -8,43 +8,43 @@ class Button
 public:
     /*! OnStateChangedEventHandler().
     \typedef OnStateChangedEventHandler().
-    \param[in] state Current state of the button
-    \return N/A
+    \param[in]  state   Current state of the button.
+    \return             N/A.
     */
     typedef void(*OnStateChangedEventHandler)(const bool state);
 
 private:
-    uint8_t m_Pin; /*!< The pin number */
-    bool m_State; /*!< Holds the last read state of the button */
+    uint8_t m_Pin; /*!< The pin number. */
+    bool m_State; /*!< Holds the last read state of the button. */
         
-    OnStateChangedEventHandler m_OnStateChangedEvent = nullptr; /*!< Pointer to callback function to fire when the button's state has changed */
+    OnStateChangedEventHandler m_OnStateChangedEvent = nullptr; /*!< Pointer to callback function to fire when the button's state has changed. */
 
 public:
     /*! GetState().
     \fn GetState().
-    \param N/A
-    \return Current state of the button
+    \param  N/A.
+    \return Current state of the button.
     */
     bool GetState(void) const;
     
     /*! SetOnStateChangedEvent().
     \fn SetOnStateChangedEvent().
-    \param[in] value Pointer to a callback function
-    \return N/A
+    \param[in]  value   Pointer to a callback function.
+    \return             N/A.
     */
     void SetOnStateChangedEvent(const OnStateChangedEventHandler value);
 
     /*! Poll().
     \fn Poll().
-    \param N/A
-    \return N/A
+    \param  N/A.
+    \return N/A.
     */
     virtual void Poll(void);
 
     /*! Button().
     \fn Button().
-    \param[in] pin The pin number to read from
-    \return N/A
+    \param[in]  pin The pin number to read from.
+    \return         N/A.
     */
     Button(const uint8_t pin);
 };
--- inc/HC06.hpp
+++ inc/HC06.hpp
@@ -34,81 +34,120 @@ private:
     
     /*! GenerateCommand().
     \fn GenerateCommand().
-    \param .
-    \return .
+    \param[out] resultBuffer    Buffer to store he resulting command in. This must have enough space to store the result.
+    \param[in]  command         The command to be prepended to the result buffer.
+    \param[in]  commandLength   The length of the command.
+    \param[in]  value           The additional part(parameters, varying string etc.) to be appended to the result buffer.
+    \param[in]  valueLength     The length of the value.
+    \return                     N/A.
     */
     static void GenerateCommand(char* const resultBuffer, const char* const command, const size_t commandLength, const char* const value, const size_t valueLength);
     
     /*! SendCommand().
     \fn SendCommand().
-    \param .
-    \return .
+    \param[in]  command         The command to be sent.
+    \param[in]  responseLength  The expected respnse length to wait for.
+    \return                     A pointer to the resulting string. This is stored in an internal buffer and is not guaranteed to persist.
     */
     const char *SendCommand(const char *const command, const int responseLength);
 
 public:
     /*! Ping().
     \fn Ping().
-    \param .
-    \return .
+    \param  N/A.
+    \return True is the device is responsive, false if not.
     */
     bool Ping(void);
 
     /*! GetVersion().
     \fn GetVersion().
-    \param .
-    \return .
+    \param  N/A.
+    \return A pointer to the resulting string containing the device's version. This is stored in an internal buffer and is not guaranteed to persist.
     */
     const char *GetVersion(void);
     
     /*! SetBaudRate().
     \fn SetBaudRate().
-    \param .
-    \return .
+    \param[in]  value   The device's desirable baud rate.
+    \return             True on success, false otherwise.
     */
     bool SetBaudRate(const HC06::baudrate_t value);
     
     /*! SetName().
     \fn SetName().
-    \param .
-    \return .
+    \param[in]  value   The device's desirable name (max. 20 characters).
+    \return             True on success, false otherwise.
     */
     bool SetName(const char *const value);
     
     /*! SetPIN().
     \fn SetPIN().
-    \param .
-    \return .
+    \param[in]  value   The device's desirable pin code (max. 4 characters).
+    \return             True on success, false otherwise.
     */
     bool SetPIN(const char *const value);
     
     /*! Begin().
     \fn Begin().
-    \param .
-    \return .
+    \param[in]  baudRate    The desirable baud rate used to communicate with the device.
+    \return                 N/A.
     */
     void Begin(const HC06::baudrate_t baudRate);
     
     /*! AvailableBytes().
     \fn AvailableBytes().
-    \param .
-    \return .
+    \param  N/A.
+    \return The number of bytes available for read.
     */
     int AvailableBytes(void);
+
+    /*! Write().
+    \fn Write().
+    \param[in]  buffer  A sequence of any characters to be written to the device.
+    \param[in]  length  The length of the buffer.
+    \return             The number of bytes written.
+    */
     size_t Write(const uint8_t *const buffer, const size_t length); /*!< TODO */
-    size_t Write(const char *const buffer, const size_t length); /*!< TODO */
-    size_t Write(const char *const str); /*!< TODO */
-    size_t Write(const uint8_t byte); /*!< TODO */
+
+    /*! Write().
+    \fn Write().
+    \param[in]  buffer  A pointer to an object/buffer to be written to the device.
+    \param[in]  length  The length of the buffer.
+    \return             The number of bytes written.
+    */
+    size_t Write(const char *const buffer, const size_t length);
+
+    /*! Write().
+    \fn Write().
+    \param[in]  str A null-terminated string to be written to the device.
+    \return         The number of bytes written.
+    */
+    size_t Write(const char *const str);
+
+    /*! Write().
+    \fn Write().
+    \param[in]  byte    A single byte to be written to the device.
+    \return             The number of bytes written.
+    */
+    size_t Write(const uint8_t byte);
     
     /*! Flush().
     \fn Flush().
-    \param .
-    \return .
+    \param  N/A.
+    \return N/A.
     */
     void Flush(void);
 
     operator bool(void);
 
+    /*! HC06().
+    \fn HC06().
+    \param[in]  rxPin           The pin number to read from the device.
+    \param[in]  txPin           The pin number to write to the device.
+    \param[in]  timeout         Communication timeout (Optional, default: 1000UL).
+    \param[in]  inverseLogic    Inverses the logic (Optional, default: false).
+    \return                     N/A.
+    */
     HC06(const uint8_t rxPin, const uint8_t txPin, const unsigned long int timeout = 1000UL, const bool inverseLogic = false);
 };
 
