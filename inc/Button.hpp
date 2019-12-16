b061f3f2096d2bde447a8adb0ef82a1a02d265a2
albrdev <albrdev@gmail.com>
2019-12-16 14:48:43 +0100

    comments++

--- inc/Button.hpp
+++ inc/Button.hpp
@@ -1,50 +1,50 @@
 #ifndef __BUTTON_HPP__
 #define __BUTTON_HPP__
 
-#include <stdint.h> /* uint8_t */
+#include <stdint.h>
 
 class Button
 {
 public:
     /*! OnStateChangedEventHandler().
-    \fn OnStateChangedEventHandler().
-    \param .
-    \return .
+    \typedef OnStateChangedEventHandler().
+    \param Current state of the button
+    \return N/A
     */
     typedef void(*OnStateChangedEventHandler)(const bool);
 
 private:
-    uint8_t m_Pin; /*!< map Pin to designated hardware pin */
-    bool m_State; /*!< setting the state */
-
-    OnStateChangedEventHandler m_OnStateChangedEvent = nullptr;
+    uint8_t m_Pin; /*!< The pin number */
+    bool m_State; /*!< Holds the last read state of the button */
+        
+    OnStateChangedEventHandler m_OnStateChangedEvent = nullptr; /*!< Pointer to callback function to fire when the button's state has changed */
 
 public:
     /*! GetState().
     \fn GetState().
-    \param .
-    \return .
+    \param N/A
+    \return Current state of the button
     */
     bool GetState(void) const;
     
     /*! SetOnStateChangedEvent().
     \fn SetOnStateChangedEvent().
-    \param .
-    \return .
+    \param Pointer to a callback function
+    \return N/A
     */
     void SetOnStateChangedEvent(const OnStateChangedEventHandler value);
 
     /*! Poll().
     \fn Poll().
-    \param .
-    \return .
+    \param N/A
+    \return N/A
     */
     virtual void Poll(void);
 
     /*! Button().
     \fn Button().
-    \param .
-    \return .
+    \param The pin number to read from
+    \return N/A
     */
     Button(const uint8_t pin);
 };
